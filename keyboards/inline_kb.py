9d139339b6d037e4ea99f37a8da94d4638b5a953
Rufinad <alsergrits@gmail.com>
2024-05-04 13:16:21 +0300

    pip freeze > requirements.txt

--- keyboards/inline_kb.py
+++ keyboards/inline_kb.py
@@ -1,29 +1,29 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-
-# Создаем объекты инлайн-кнопок
-button_1 = InlineKeyboardButton(
-    text='FTS',
-    callback_data='FTS'
-)
-button_2 = InlineKeyboardButton(
-    text='Sigma',
-    callback_data='Sigma'
-)
-button_3 = InlineKeyboardButton(
-    text='Portnews',
-    callback_data='Portnews'
-)
-button_4 = InlineKeyboardButton(
-    text='Custom',
-    callback_data='Custom'
-)
-
-# Создаем объект инлайн-клавиатуры
-start_keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[[button_1],
-                     [button_2],
-                     [button_3],
-                     [button_4],
-                     ])
-
+# from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+# from aiogram.utils.keyboard import InlineKeyboardBuilder
+#
+# # Создаем объекты инлайн-кнопок
+# button_1 = InlineKeyboardButton(
+#     text='FTS',
+#     callback_data='FTS'
+# )
+# button_2 = InlineKeyboardButton(
+#     text='Sigma',
+#     callback_data='Sigma'
+# )
+# button_3 = InlineKeyboardButton(
+#     text='Portnews',
+#     callback_data='Portnews'
+# )
+# button_4 = InlineKeyboardButton(
+#     text='Custom',
+#     callback_data='Custom'
+# )
+#
+# # Создаем объект инлайн-клавиатуры
+# start_keyboard = InlineKeyboardMarkup(
+#     inline_keyboard=[[button_1],
+#                      [button_2],
+#                      [button_3],
+#                      [button_4],
+#                      ])
+#
--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -7,9 +7,9 @@ async def set_main_menu(bot: Bot):
 
     # Создаем список с командами и их описанием для кнопки menu
     main_menu_commands = [
-        BotCommand(command='/change_info',
-                   description='парсинг сайта ved.today'),
-        BotCommand(command='/get_horoscope',
-                   description='парсинг сайта sigma-soft'),
+        BotCommand(command='/start',
+                   description='Начать подборку гардероба'),
+        # BotCommand(command='/get_horoscope',
+        #            description='парсинг сайта sigma-soft'),
     ]
     await bot.set_my_commands(main_menu_commands)
